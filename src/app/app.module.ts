3845319765a51299c4846611a03f059af73b607e
PKM007 <prakhar.prakhar.mishra1@gmail.com>
2023-03-26 16:15:02 +0530

    cleanup fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './hotel-management/header/header.component';
 import { NgImageSliderModule } from 'ng-image-slider';
 import { HotelManagementModule } from './hotel-management/hotel-management.module';
-import { MainLandingComponent } from './hotel-management/main-landing/main-landing.component';
-import { FooterComponent } from './hotel-management/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainNavComponent } from './hotel-management/main-nav/main-nav.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -17,26 +13,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    MainLandingComponent,
-    FooterComponent,
-    MainNavComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgImageSliderModule,
     BrowserAnimationsModule,
     HotelManagementModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
   ],
-  providers: [NgImageSliderModule, HotelManagementModule],
+  providers: [NgImageSliderModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/hotel-management/hotel-management.module.ts
+++ src/app/hotel-management/hotel-management.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgImageSliderModule } from 'ng-image-slider';
+import { MainLandingComponent } from './main-landing/main-landing.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
+import { FooterComponent } from './footer/footer.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -10,7 +12,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [],
+  declarations: [MainLandingComponent, FooterComponent, MainNavComponent],
   imports: [
     CommonModule,
     NgImageSliderModule,
@@ -21,5 +23,6 @@ import { MatListModule } from '@angular/material/list';
     MatIconModule,
     MatListModule,
   ],
+  exports: [MainLandingComponent, FooterComponent, MainNavComponent],
 })
 export class HotelManagementModule {}
